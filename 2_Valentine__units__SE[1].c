e46c6446a3a0b6d8688d56870b32a43f6fa1871d
ogutuvalentine6-a11y <ogutuvalentine6@gmail.com>
2025-09-20 04:54:51 -0700

    Add files via upload

---
+++ 2_Valentine__units__SE[1].c
@@ -0,0 +1,21 @@
+// simple SE units 
+ /*
+ Name : valentine otieno ogutu 
+ Reg no : PA106/28813/25
+ Description: Simple Program that displays software engineering semester one,year one units 
+  */
+  #include<stdio.h> // preprocessing directive: scanf(),
+  
+  // main function 
+  int main () {
+      printf("Discrete Mathematics\n");
+      printf("Mathematics for science\n");
+      printf("Fundermental of ICT\n");
+      printf("Community skills\n");
+      printf("Computer systems and Organization\n");
+      printf("Introduction to programming/ Problem solving\n");
+      printf("Fundermental of Systems Analysis and Design\n");
+      
+      return 0;
+      }
+      
---
+++ Bank_account[1].c
@@ -0,0 +1,31 @@
+// simple Bank balance Program 
+ /*
+ Name : valentine otieno ogutu 
+ Reg no : PA106/28813/25
+ Description: Simple Program that displays Bank Account 
+  */
+  #include<stdio.h> // preprocessing directive: scanf(),
+  
+  // main function 
+  int main () {
+      int age;
+      int  IDnumber ;
+      int  Height  ;
+      int Bankbalance ;
+      
+      printf("enter your age \n"); 
+      scanf("%d",&age); 
+      printf("your age is %d",age); 
+      
+      printf("enter your IDnumber\n");
+      scanf("%d",&IDnumber);
+      printf("your IDnumber is %d",IDnumber);
+      
+      printf("enter your Bankbalance\n");
+      scanf("%d" ,&Bankbalance);
+      printf("Your Bankbalance is %d",Bankbalance);
+      
+      return 0 ;
+      }
+      
+      
