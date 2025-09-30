8409b98ed75e56e9838a28b24eac31fd491f4cb8
VALENTINE <ogutuvalentine6@gmail.com>
2025-09-30 20:37:03 +0300

    Add files via upload

---
+++ Water_bill_calc.c
@@ -0,0 +1,36 @@
+//Program to output the water bill
+/*
+Name:
+Reg:
+Description:
+*/
+
+#include<stdio.h>
+int main(){
+    int water_units;
+    float bills;
+
+    printf("Enter number of water units:");
+    scanf("%d",&water_units);
+
+    if (water_units>0&&water_units<=30)
+    {
+      bills=20*water_units;
+      printf("your total bill is %.2f",bills);
+    }
+    else if (water_units>=31&&water_units<=60)
+    {
+        bills=25*water_units;
+        printf("your total bill is%.2f",bills);
+    }
+    else if (water_units>60)
+    {
+       bills=30*water_units;
+       printf("your total bill is%.2f",bills);
+    }
+    else{
+        printf("invalid choice");
+    }
+    
+    return 0;
+}
