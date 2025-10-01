3c15a158430fb63b1be0610b885249f01de64ca7
VALENTINE <ogutuvalentine6@gmail.com>
2025-10-01 19:01:08 +0300

    Add files via upload

---
+++ Data_bundles (1).c
@@ -0,0 +1,41 @@
+//A program to display offers of data bundles
+/*
+Name:Elijah kiprotich ngetich
+Reg:pa106/g/28708/25
+Description:
+*/
+
+#include<stdio.h>
+int main(){
+    int choice;
+
+    printf("Select data bundles:\n",choice);
+    printf("1.100MB @ 50 KES\n",choice);
+    printf("2.500MB @ 200 KES\n",choice);
+    printf("3.1GB @ 350 KES\n",choice);
+    printf("4.2GB @ 600 KES\n",choice);
+
+    printf("Enter your choice(1-4):\n");
+    scanf("%d",&choice);
+
+    if (choice==1)
+    {
+        printf("Your selected 100MB");
+    }
+    else if (choice==2)
+    {
+        printf("Your selected 500MB");
+    }
+    else if (choice==3)
+    {
+        printf("you selected 1GB");
+    }
+    else if (choice==4)
+    {
+        printf("Your selected 2GB");
+    }
+    else {
+        printf("Invalid choice");
+    }
+  return 0;  
+}
