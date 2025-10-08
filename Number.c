a586944c89f4274aadf146123ff5a279dac592ff
VALENTINE <ogutuvalentine6@gmail.com>
2025-10-08 12:03:07 +0300

    Add files via upload

---
+++ Number.c
@@ -0,0 +1,17 @@
+//Print the numbers from 100 to 50
+/*
+Name:Elijah Kiprotich Ngétich
+Registration:PA106/G/28708/25
+Describtion:
+*/ 
+
+#include<stdio.h>
+int main()
+{
+    int i;
+    for (i=100;i>=50;i--)
+    {
+        printf("%d\t",i);
+    }
+  return 0;  
+}
