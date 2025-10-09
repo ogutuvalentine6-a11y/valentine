5bfd4d931f76f876e3dbaf443aad995207888f05
VALENTINE <ogutuvalentine6@gmail.com>
2025-10-09 19:29:56 +0300

    Add files via upload

---
+++ for_loop (1).c
@@ -0,0 +1,21 @@
+/*
+NAME:Felix Onyancha Moinde
+REG NO:PA106/G/28821/25
+DATE:8th Oct 2025
+DESCRIPTION:Program to print 100-50 in descending order
+*/
+
+#include <stdio.h>
+
+int main()
+{
+	//for(start; stop; step){}
+	//for(initializing; condition; increment){}
+	int i,x;
+	for(i=100; i>=50; i--){
+		printf("%d\n", i);
+	}
+
+	
+	return 0;
+}
