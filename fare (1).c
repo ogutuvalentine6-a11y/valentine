17dfb8a3de98645569d07f1ec0d5a84c9ac8b00e
VALENTINE <ogutuvalentine6@gmail.com>
2025-10-23 12:23:16 +0300

    Add files via upload

---
+++ fare (1).c
@@ -0,0 +1,24 @@
+/*
+NAME:Felix Onyancha Moinde 
+REG NO:PA106/G/28821/25
+DESCRIPTION:A program to calculate the total fare per the distance covered
+*/
+
+#include <stdio.h>
+//calculatefare() function
+float calculatefare(int distance){
+    //fare is Ksh. 50 per km
+    float fare = distance * 50;
+    
+    return fare;
+}
+
+int main(){
+    float distance;
+    //prompt the user to enter the distance covered
+    printf("Enter the distance covered in km: ");
+    scanf("%f", &distance);
+    //display the total fare 
+    printf("The tatal fare is Ksh. %.2f\n", calculatefare(distance));
+    return 0;
+}
